9afbbe9d0d281eb4c6d9a3b7a132b4131e11c020
vishnu <vishnu14july@gmail.com>
2021-02-24 13:13:33 +0530

    added Tensor Class

---
+++ src/CppTorch.h
@@ -0,0 +1,4 @@
+#include "tensor/tensor.h"
+#include <xtensor/xio.hpp>
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xview.hpp>
---
+++ src/tensor/tensor.h
@@ -0,0 +1,13 @@
+#ifndef _TENSOR_H_
+#define _TENSOR_H_
+
+#include <xtensor/xarray.hpp>
+using namespace xt;
+
+template <class T,
+          layout_type L = XTENSOR_DEFAULT_LAYOUT,
+          class A = XTENSOR_DEFAULT_ALLOCATOR(T),
+          class SA = std::allocator<typename std::vector<T, A>::size_type>>
+using Tensor = xarray<T, L, A, SA>;
+
+#endif
