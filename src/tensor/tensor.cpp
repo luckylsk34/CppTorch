7d6a2772a87208432b0e03dc3a2cef3440f7f15b
vishnu <vishnu14july@gmail.com>
2021-02-26 00:48:22 +0530

    loss

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -1,1 +1,12 @@
 #include "tensor.h"
+
+// template <class T,
+//           layout_type L = XTENSOR_DEFAULT_LAYOUT,
+//           class A = XTENSOR_DEFAULT_ALLOCATOR(T),
+//           class SA = std::allocator<typename std::vector<T, A>::size_type>>
+// Tensor Tensor::operator[](int index){
+
+//     return xt::view(this, index, xt::all());
+
+
+// }
--- src/tensor/tensor.h
+++ src/tensor/tensor.h
@@ -4,10 +4,19 @@
 #include <xtensor/xarray.hpp>
 using namespace xt;
 
+// template <class T,
+//           layout_type L = XTENSOR_DEFAULT_LAYOUT,
+//           class A = XTENSOR_DEFAULT_ALLOCATOR(T),
+//           class SA = std::allocator<typename std::vector<T, A>::size_type>>
+// using Tensor = xarray<T, L, A, SA>;
 template <class T,
           layout_type L = XTENSOR_DEFAULT_LAYOUT,
           class A = XTENSOR_DEFAULT_ALLOCATOR(T),
           class SA = std::allocator<typename std::vector<T, A>::size_type>>
-using Tensor = xarray<T, L, A, SA>;
+class Tensor : xarray <T, L, A, SA>
+{
+public:
+	using xarray<T, L, A, SA>::xarray_container;
+};
 
 #endif
