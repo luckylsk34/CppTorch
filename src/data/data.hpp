1a193d146d4e44d8b65010470fd7a8b37f964e1d
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2021-02-26 02:29:01 +0530

    added batching to dataloader.

--- src/data/data.hpp
+++ src/data/data.hpp
@@ -22,29 +22,46 @@ class DataLoader
 {
 public:
 	Dataset<T> *dataset;
+	int batch_size;
 
-	DataLoader(Dataset<T> &dataset)
-		: dataset(&dataset) {};
+	DataLoader(Dataset<T> &dataset, int batch_size = 1)
+		: dataset(&dataset), batch_size(batch_size) {};
 
 	template <typename S>
 	class iterator
 	{
-	public:
 		int index = 0;
 		Dataset<S> *dataset;
+		int batch_size;
 
-		iterator(int index, Dataset<S> *dataset)
-			: index(index), dataset(dataset) {};
+	public:
+		iterator(int index, Dataset<S> *dataset, int batch_size)
+			: index(index), dataset(dataset), batch_size(batch_size) {};
 
 		inline void operator++() { index++; };
 		inline void operator++(int) { index++; };
-		inline bool operator==(iterator &rhs) { return index == rhs.index; }
-		inline bool operator!=(iterator &rhs) { return index != rhs.index; }
-		inline auto operator*() { return (*dataset)[index]; }
+		inline bool operator==(iterator &rhs) { return index >= rhs.index; }
+		inline bool operator!=(iterator &rhs) { return index < rhs.index; }
+		inline auto operator*()
+		{
+			auto [x, y] = (*dataset)[index];
+			x = xt::expand_dims(x, 0);
+			y = xt::expand_dims(y, 0);
+
+			for (int i = 1; i < batch_size && ++index < dataset->size(); i++) {
+				auto [x1, y1] = (*dataset)[index];
+				x1 = xt::expand_dims(x1, 0);
+				y1 = xt::expand_dims(y1, 0);
+				x = xt::concatenate(xt::xtuple(x, x1), 0);
+				y = xt::concatenate(xt::xtuple(y, y1), 0);
+			}
+
+			return make_tuple(x, y);
+		}
 	};
 
-	auto begin() { return iterator<T>(0, dataset); }
-	auto end() { return iterator<T>(dataset->size(), dataset); }
+	auto begin() { return iterator<T>(0, dataset, batch_size); }
+	auto end() { return iterator<T>(dataset->size(), dataset, batch_size); }
 };
 
 typedef uint8_t MNIST_Dtype;
@@ -59,7 +76,8 @@ public:
 	int size();
 	TrainingPair<MNIST_Dtype> operator[](int index)
 	{
-		return { xt::view(images, index, xt::all()), xt::view(labels, index, xt::all()) };
+		return { (Tensor<MNIST_Dtype>) xt::view(images, index, xt::all()),
+			     (Tensor<MNIST_Dtype>) xt::view(labels, index, xt::all()) };
 	};
 };
 #endif
--- tests/dataTest.cpp
+++ tests/dataTest.cpp
@@ -7,12 +7,13 @@ int main()
 	std::string images_file = "datasets\\mnist\\train-images.idx3-ubyte";
 	std::string labels_file = "datasets\\mnist\\train-labels.idx1-ubyte";
 	MNISTDataset mnistDataset(images_file, labels_file);
-	DataLoader dataLoader(mnistDataset);
+	DataLoader dataLoader(mnistDataset, 16);
 
 	xt::print_options::set_line_width(200);
 	for (auto batch : dataLoader) {
 		auto [x, y] = batch;
-		std::cout << x << y << std::endl;
+		std::cout << xt::adapt(x.shape())
+				  << xt::adapt(y.shape()) << std::endl;
 	}
 	std::cout << "\n";
 	return 0;
