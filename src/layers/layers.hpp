620bd3d4f83383aaea363bb3cbfd97a9fb5cd694
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2021-02-27 20:37:14 +0530

    fixed layers code and its tests

--- src/layers/layers.hpp
+++ src/layers/layers.hpp
@@ -1,8 +1,8 @@
 #ifndef _LAYERS_H_
 #define _LAYERS_H_
 
-#include <map>
 #include "../tensor/tensor.hpp"
+#include <map>
 
 template <typename T>
 using tensordict = std::map<std::string, Tensor<T>>;
@@ -11,56 +11,55 @@ template <typename T>
 class Layer
 {
 public:
-    tensordict<T> params;
-    tensordict<T> grads;
+	tensordict<T> params;
+	tensordict<T> grads;
 
-    virtual Tensor<T> forward(Tensor<T> inputs) = 0;
-    virtual Tensor<T> backward(Tensor<T> grad) = 0;
+	virtual Tensor<T> forward(Tensor<T> input) = 0;
+	virtual Tensor<T> backward(Tensor<T> grad) = 0;
+
+	inline auto operator()(Tensor<T> input)
+	{
+		return forward(input);
+	}
 };
 
 template <typename T>
-class Linear : Layer<T>
+class Linear : public Layer<T>
 {
 public:
-    Tensor<T> inputs;
-    Linear(int input_size, int output_size)
-    {
-        this->params["w"] = xt::random::randn<T>({input_size, output_size});
-        this->params["b"] = xt::random::randn<T>({output_size});
-    }
+	Tensor<T> inputs;
+	Linear(int input_size, int output_size)
+	{
+		this->params["w"] = xt::eval(xt::random::randn<T>({ input_size, output_size }));
+		this->params["b"] = xt::eval(xt::random::randn<T>({ output_size }));
+	}
 
-    Tensor<T> forward(Tensor<T> input) override
-    {
-        this->inputs = input;
-        auto res = xt::linalg::dot(inputs, this->params["w"]);
+	Tensor<T> forward(Tensor<T> input) override
+	{
+		this->inputs = input;
+		auto res = xt::linalg::dot(input, this->params["w"]);
 
-        return res + this->params["b"];
-    }
-    Tensor<T> backward(Tensor<T> grad) override
-    {
-        this->grads["b"] = xt::sum(grad, 0);
-        this->grads["w"] = xt::linalg::dot(this->inputs, grad);
+		return res + this->params["b"];
+	}
 
-        return xt::linalg::dot(grad, xt::transpose(this->params["w"]));
-    }
+	Tensor<T> backward(Tensor<T> grad) override
+	{
+		this->grads["b"] = xt::sum(grad, 0);
+		this->grads["w"] = xt::linalg::dot(xt::transpose(this->inputs), grad);
 
-    ~Linear()
-    {
-        this->params.clear();
-        this->grads.clear();
-    }
+		return xt::linalg::dot(grad, xt::transpose(this->params["w"]));
+	}
+
+	~Linear()
+	{
+		this->params.clear();
+		this->grads.clear();
+	}
 };
 
-template<typename T>
-class Activation: Layer <T>
+template <typename T>
+class Activation : Layer<T>
 {
-    
-
-
-
-
-
 };
-
 
 #endif
--- tests/layerTest.cpp
+++ tests/layerTest.cpp
@@ -4,17 +4,17 @@ using namespace xt;
 
 int main(int argc, char *argv[])
 {
-	Tensor<double> arr1{{1.0, 2.0, 3.0},
-						{2.0, 5.0, 7.0},
-						{2.0, 5.0, 7.0}};
-	Tensor<double> arr2{5.0, 6.0, 7.0};
-	// std::cout << xt::view(arr1, 0, xt::all()) << std::endl;
-	// std::cout << xt::pow(arr1, 2)<< std::endl;
-	Tensor<double> pred{5.0, 6.0, 7.0};
-	Tensor<double> act{1.0, 6.0, 7.0};
-    Linear <double> linear(3,4);
+	Tensor<double> arr1 { { 1.0, 2.0, 3.0 },
+		                  { 2.0, 5.0, 7.0 },
+		                  { 2.0, 5.0, 7.0 } };
+	Tensor<double> arr2 { 5.0, 6.0, 7.0 };
 
-    std::cout << linear.forward(arr1) << std::endl;
+	Linear<double> linear(3, 4);
+	std::cout << linear(arr1) << std::endl;
+	Tensor<double> grad { { 1.0, 2.0, 3.0, 4.0 },
+		                  { 2.0, 5.0, 7.0, 4.0 },
+		                  { 2.0, 5.0, 7.0, 4.0 } };
+	std::cout << linear.backward(grad) << std::endl;
 
 	return 0;
 }
