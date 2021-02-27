568f32dca749fd913638a24f024d0539abbbbe14
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2021-02-27 20:35:18 +0530

    cleaned up code

--- tests/lossTest.cpp
+++ tests/lossTest.cpp
@@ -1,22 +1,14 @@
 #include "../src/loss/loss.hpp"
+#include <assert.h>
 
 using namespace xt;
 
 int main(int argc, char *argv[])
 {
-	Tensor<double> arr1{{1.0, 2.0, 3.0},
-						{2.0, 5.0, 7.0},
-						{2.0, 5.0, 7.0}};
-	Tensor<double> arr2{5.0, 6.0, 7.0};
-	// std::cout << xt::view(arr1, 0, xt::all()) << std::endl;
-	// std::cout << xt::pow(arr1, 2)<< std::endl;
-	Tensor<double> pred{5.0, 6.0, 7.0};
-	Tensor<double> act{1.0, 6.0, 7.0};
+	Tensor<double> pred { 5.0, 6.0, 7.0 };
+	Tensor<double> act { 1.0, 6.0, 7.0 };
 	MSE mse_loss = MSE();
-	std::cout << mse_loss(pred,act) << std::endl;
-	std::cout <<  xt::transpose(arr1) << std::endl;
-	std::cout << xt::linalg::dot(arr1, arr1) +  arr2<< std::endl;
-	std::cout << xt::random::randn<double>({3, 3}) << std::endl;
+	assert(mse_loss(pred, act)[0] == 16);
 
 	return 0;
 }
