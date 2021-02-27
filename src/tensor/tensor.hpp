d2e83a1a1e0b1dbf918f05a20e180211937cc7b6
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2021-02-28 02:14:52 +0530

    fixed code

--- src/tensor/tensor.hpp
+++ src/tensor/tensor.hpp
@@ -1,15 +1,16 @@
 #ifndef _TENSOR_H_
 #define _TENSOR_H_
 
-#include <xtensor/xarray.hpp>
-#include <xtensor/xio.hpp>
-#include <xtensor/xtensor.hpp>
-#include <xtensor/xview.hpp>
-#include <xtensor/xrandom.hpp>
-#include <xtensor/xadapt.hpp>
-#include <xtensor/xmath.hpp>
 #include <xtensor-blas/xblas.hpp>
 #include <xtensor-blas/xlinalg.hpp>
+#include <xtensor/xadapt.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xio.hpp>
+#include <xtensor/xmath.hpp>
+#include <xtensor/xrandom.hpp>
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xview.hpp>
+
 using namespace xt;
 
 template <class T,
@@ -17,14 +18,5 @@ template <class T,
           class A = XTENSOR_DEFAULT_ALLOCATOR(T),
           class SA = std::allocator<typename std::vector<T, A>::size_type>>
 using Tensor = xarray<T, L, A, SA>;
-// template <class T,
-//           layout_type L = XTENSOR_DEFAULT_LAYOUT,
-//           class A = XTENSOR_DEFAULT_ALLOCATOR(T),
-//           class SA = std::allocator<typename std::vector<T, A>::size_type>>
-// class Tensor : xarray <T, L, A, SA>
-// {
-// public:
-// 	using xarray<T, L, A, SA>::xarray_container;
-// };
 
 #endif
